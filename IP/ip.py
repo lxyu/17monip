41cf97eeaf605994dcff6a96476faa4f576f2a06
Lx Yu <i@lxyu.net>
2014-08-06 13:17:25 +0800

    some performance tuning
    thanks for @sept

--- IP/ip.py
+++ IP/ip.py
@@ -16,6 +16,7 @@ _unpack_C = lambda b: struct.unpack("B", b)
 with open(os.path.join(os.path.dirname(__file__), "17monipdb.dat"), "rb") as f:
     dat = f.read()
     offset, = _unpack_N(dat[:4])
+    max_comp_len = offset - 1028
     index = dat[4:offset]
 
 
@@ -41,12 +42,11 @@ def _find_ip(ip):
     start, = _unpack_V(index[tmp_offset:tmp_offset + 4])
 
     index_offset = index_length = 0
-    max_comp_len = offset - 1028
     start = start * 8 + 1024
     while start < max_comp_len:
         if index[start:start + 4] >= nip:
             index_offset, = _unpack_V(
-                index[start + 4:start + 7] + chr(0).encode("utf-8"))
+                index[start + 4:start + 7] + b'\0')
             if PY2:
                 index_length, = _unpack_C(index[start + 7])
             else:
